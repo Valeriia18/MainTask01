3a8d2a8da661f4159407f07e0a80f993169dadb3
Valeriia18 <45722123+Valeriia18@users.noreply.github.com>
2019-01-06 19:17:11 +0300

    Update ConsolePrinter.java

--- src/ConsolePrinter.java
+++ src/ConsolePrinter.java
@@ -4,12 +4,14 @@ package by.epam.javatraining.ValeriiaAmialchenia.maintask01;
  * Class ConsolePrinter implements print method 
 
  
-  @version 1.0 26 Dec 2018
+    @version 1.0 26 Dec 2018
+    @version 2.0 06 Jan 2019
   @author Valeriia Amialchenia
  */
 public class ConsolePrinter {
-public static void printArray (String msgArray){
-    
-    System.out.println(msgArray);
-}
+
+    public static void printArray(String msgArray) {
+
+        System.out.println(msgArray);
+    }
 }
