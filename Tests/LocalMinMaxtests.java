1289aaa36c5f31d1c0f4a2ba7e6bbc5ad1ecb9c5
Valeriia18 <45722123+Valeriia18@users.noreply.github.com>
2019-01-06 19:19:51 +0300

    Update LocalMinMaxtests.java

--- Tests/LocalMinMaxtests.java
+++ Tests/LocalMinMaxtests.java
@@ -1,6 +1,8 @@
 package by.epam.javatraining.ValeriiaAmialchenia.maintask01.tests;
 
 import by.epam.javatraining.ValeriiaAmialchenia.maintask01.Logic;
+import by.epam.javatraining.ValeriiaAmialchenia.maintask01.NullArrayException;
+import by.epam.javatraining.ValeriiaAmialchenia.maintask01.ZeroLengthArrayException;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -8,97 +10,92 @@ import static org.junit.Assert.*;
  * Tests for searching for the index of first local minimums or maximums in a given array.
  *
  * @version 1.0 26 Dec 2018
+ * @version 2.0 06 Jan 2019
  * @author Valeriia Amialchenia
  */
 public class LocalMinMaxtests {
 
     @Test
-    public void findLocalMax1() {
+    public void findLocalMax1() throws NullArrayException, ZeroLengthArrayException {
         int[] arrayIn = {-1, 0, -3, 3, 1, 6};
         assertEquals(1, Logic.findLocalMax(arrayIn));
     }
 
     @Test
-    public void findLocalMax2() {
+    public void findLocalMax2() throws NullArrayException, ZeroLengthArrayException {
         int[] arrayIn = {15, 3};
         assertEquals(-1, Logic.findLocalMax(arrayIn));
     }
 
     @Test
-    public void findLocalMax3() {
+    public void findLocalMax3() throws NullArrayException, ZeroLengthArrayException {
         int[] arrayIn = {3};
         assertEquals(-1, Logic.findLocalMax(arrayIn));
     }
 
     @Test
-    public void findLocalMax4() {
-        int[] arrayIn = {0, 0, 0};
-        assertEquals(-1, Logic.findLocalMax(arrayIn));
+    public void findLocalMax4() throws NullArrayException, ZeroLengthArrayException {
+        int[] arrayIn = new int[]{0, 0, 0};
+        Logic.findLocalMax(arrayIn);
     }
 
     @Test
-    public void findLocalMax5() {
+    public void findLocalMax5() throws NullArrayException, ZeroLengthArrayException {
         int[] arrayIn = {-4, -1, -7};
         assertEquals(1, Logic.findLocalMax(arrayIn));
     }
 
     @Test
-    public void findLocalMax6() {
-        int[] arrayIn = {};
-        assertEquals(-1, Logic.findLocalMax(arrayIn));
-    }
-
-    @Test
-    public void findLocalMax7() {
+    public void findLocalMax6() throws NullArrayException, ZeroLengthArrayException {
         int[] arrayIn = null;
-        assertEquals(-1, Logic.findLocalMax(arrayIn));
+        assertNull(arrayIn);
     }
 
     @Test
-    public void findLocalMin1() {
+    public void findLocalMin1() throws NullArrayException, ZeroLengthArrayException {
         int[] arrayIn = {5, 10, 3, 12};
         assertEquals(2, Logic.findLocalMin(arrayIn));
     }
 
     @Test
-    public void findLocalMin2() {
+    public void findLocalMin2() throws NullArrayException, ZeroLengthArrayException {
         int[] arrayIn = {-1, -100, -10};
         assertEquals(1, Logic.findLocalMin(arrayIn));
     }
 
     @Test
-    public void findLocalMin3() {
+    public void findLocalMin3() throws NullArrayException, ZeroLengthArrayException {
         int[] arrayIn = {-7, -4};
         assertEquals(-1, Logic.findLocalMin(arrayIn));
     }
 
     @Test
-    public void findLocalMin4() {
+    public void findLocalMin4() throws NullArrayException, ZeroLengthArrayException {
         int[] arrayIn = {7, 4};
         assertEquals(-1, Logic.findLocalMin(arrayIn));
     }
 
     @Test
-    public void findLocalMin5() {
+    public void findLocalMin5() throws NullArrayException, ZeroLengthArrayException {
         int[] arrayIn = {7};
         assertEquals(-1, Logic.findLocalMin(arrayIn));
     }
 
     @Test
-    public void findLocalMin6() {
+    public void findLocalMin6() throws NullArrayException, ZeroLengthArrayException {
         int[] arrayIn = {0, 0, 5, 25, 15, 20, 0};
         assertEquals(4, Logic.findLocalMin(arrayIn));
     }
 
     @Test
-    public void findLocalMin7() {
-        int[] arrayIn = {};
-        assertEquals(-1, Logic.findLocalMin(arrayIn));
+    public void findLocalMin7() throws NullArrayException, ZeroLengthArrayException {
+      int[] arrayIn = null;
+        assertNull(arrayIn);
     }
-
-    @Test
-    public void findLocalMin8() {
-        int[] arrayIn = null;
-        assertEquals(-1, Logic.findLocalMin(arrayIn));
+     @Test
+    public void findLocalMin8() throws NullArrayException, ZeroLengthArrayException {
+        int[] arrayIn = new int[]{0, 0, 0};
+        Logic.findLocalMin(arrayIn);
     }
+    
 }
