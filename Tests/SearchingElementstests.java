ffd4e13c2f856b05cc6fa500693c110859f7769e
Valeriia18 <45722123+Valeriia18@users.noreply.github.com>
2019-01-06 19:20:53 +0300

    Update SearchingElementstests.java

--- Tests/SearchingElementstests.java
+++ Tests/SearchingElementstests.java
@@ -1,6 +1,8 @@
 package by.epam.javatraining.ValeriiaAmialchenia.maintask01.tests;
 
 import by.epam.javatraining.ValeriiaAmialchenia.maintask01.Logic;
+import by.epam.javatraining.ValeriiaAmialchenia.maintask01.NullArrayException;
+import by.epam.javatraining.ValeriiaAmialchenia.maintask01.ZeroLengthArrayException;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -8,42 +10,43 @@ import static org.junit.Assert.*;
  * Tests for searching elements in a given array by Linear and Binary methods of search.
  *
  * @version 1.0 26 Dec 2018
+ * @version 2.0 06 Jan 2019
  * @author Valeriia Amialchenia
  */
 public class SearchingElementstests {
 
     @Test
-    public void linearSearchElement1() {
+    public void linearSearchElement1() throws NullArrayException, ZeroLengthArrayException {
         int[] arrayIn = {2, 4, 5, 6, 1};
         assertEquals(0, Logic.searchElementLinear(arrayIn, 2));
     }
 
     @Test
-    public void linearSearchElement2() {
+    public void linearSearchElement2() throws NullArrayException, ZeroLengthArrayException {
         int[] arrayIn = {2, 4, 5, 6, 1};
         assertEquals(-1, Logic.searchElementLinear(arrayIn, 7));
     }
 
     @Test
-    public void linearSearchElement3() {
+    public void linearSearchElement3() throws NullArrayException, ZeroLengthArrayException {
         int[] arrayIn = {6};
         assertEquals(0, Logic.searchElementLinear(arrayIn, 6));
     }
 
     @Test
-    public void linearSearchElement4() {
-        int[] arrayIn = {};
-        assertEquals(-1, Logic.searchElementLinear(arrayIn, 2));
+    public void linearSearchElement4() throws NullArrayException, ZeroLengthArrayException {
+        int[] arrayIn = new int[]{0, 0, 0};
+        Logic.searchElementLinear(arrayIn, 1);
     }
 
     @Test
-    public void linearSearchElement5() {
+    public void linearSearchElement5() throws NullArrayException, ZeroLengthArrayException {
         int[] arrayIn = null;
-        assertEquals(-1, Logic.searchElementLinear(arrayIn, 2));
+        assertNull(arrayIn);
     }
 
     @Test
-    public void binarySearchElement1() {
+    public void binarySearchElement1() throws NullArrayException, ZeroLengthArrayException {
         int[] arrayIn = {-1, 25, 10, 4};
         int[] sortedArray = Logic.bubbleSort(arrayIn);
         assertEquals(1,
@@ -51,7 +54,7 @@ public class SearchingElementstests {
     }
 
     @Test
-    public void binarySearchElement2() {
+    public void binarySearchElement2() throws NullArrayException, ZeroLengthArrayException {
         int[] arrayIn = {1, 25, 10, 4};
         int[] sortedArray = Logic.bubbleSort(arrayIn);
         assertEquals(0,
@@ -59,15 +62,15 @@ public class SearchingElementstests {
     }
 
     @Test
-    public void binarySearchElement3() {
-        int[] arrayIn = {};
-        assertEquals(-1, Logic.searchElementBinary(arrayIn, 1));
+    public void binarySearchElement3() throws NullArrayException, ZeroLengthArrayException {
+        int[] arrayIn = new int[]{0, 0, 0};
+        Logic.searchElementBinary(arrayIn, 1);
     }
 
     @Test
-    public void binarySearchElement4() {
+    public void binarySearchElement4() throws NullArrayException, ZeroLengthArrayException {
         int[] arrayIn = null;
-        assertEquals(-1, Logic.searchElementBinary(arrayIn, 1));
+        assertNull(arrayIn);
     }
 
 }
