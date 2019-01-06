4076a3306dd62b61223c26e1f1374e95d5ce7f76
Valeriia18 <45722123+Valeriia18@users.noreply.github.com>
2019-01-06 19:21:15 +0300

    Update SortingArraystests.java

--- Tests/SortingArraystests.java
+++ Tests/SortingArraystests.java
@@ -2,6 +2,8 @@ package by.epam.javatraining.ValeriiaAmialchenia.maintask01.tests;
 
 import java.util.Random;
 import by.epam.javatraining.ValeriiaAmialchenia.maintask01.Logic;
+import by.epam.javatraining.ValeriiaAmialchenia.maintask01.NullArrayException;
+import by.epam.javatraining.ValeriiaAmialchenia.maintask01.ZeroLengthArrayException;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -16,6 +18,7 @@ import static org.junit.Assert.*;
 
  *
  * @version 1.0 26 Dec 2018
+ * @version 2.0 06 Jan 2019
  * @author Valeriia Amialchenia
  */
 public class SortingArraystests {
@@ -23,133 +26,133 @@ public class SortingArraystests {
     public static final Random RANDOM = new Random();
 
     @Test
-    public void bubbleSort1() {
+    public void bubbleSort1() throws  NullArrayException, ZeroLengthArrayException {
         int[] arrayIn = {6, 4, 1, 9, 4, 8, 3};
         assertArrayEquals(new int[]{1, 3, 4, 4, 6, 8, 9}, Logic.bubbleSort(arrayIn));
     }
 
     @Test
-    public void bubbleSort2() {
+    public void bubbleSort2() throws  NullArrayException, ZeroLengthArrayException {
         int[] arrayIn = {-2, -10, -3, 0, 6, 3};
         assertArrayEquals(new int[]{-10, -3, -2, 0, 3, 6}, Logic.bubbleSort(arrayIn));
     }
 
     @Test
-    public void bubbleSort3() {
+    public void bubbleSort3() throws  NullArrayException, ZeroLengthArrayException {
         int[] arrayIn = {-3, 0, 2};
         assertArrayEquals(new int[]{-3, 0, 2}, Logic.bubbleSort(arrayIn));
     }
 
     @Test
-    public void bubbleSort4() {
-        int[] arrayIn = {};
-        assertArrayEquals(null, Logic.bubbleSort(arrayIn));
+    public void bubbleSort4() throws  NullArrayException, ZeroLengthArrayException {
+       int[] arrayIn = new int[]{0, 0, 0};
+        Logic.bubbleSort(arrayIn);
     }
 
     @Test
-    public void bubbleSort5() {
+    public void bubbleSort5() throws  NullArrayException, ZeroLengthArrayException {
         int[] arrayIn = null;
-        assertArrayEquals(null, Logic.bubbleSort(arrayIn));
+        assertNull(arrayIn);
     }
 
     @Test
-    public void shakerSort1() {
+    public void shakerSort1() throws  NullArrayException, ZeroLengthArrayException {
         int[] arrayIn = {-3, 0, 2};
         assertArrayEquals(new int[]{-3, 0, 2}, Logic.shakerSort(arrayIn));
     }
 
     @Test
-    public void shakerSort2() {
+    public void shakerSort2() throws  NullArrayException, ZeroLengthArrayException {
         int[] arrayIn = {0, 4, 1, -5, 7};
         assertArrayEquals(new int[]{-5, 0, 1, 4, 7}, Logic.shakerSort(arrayIn));
     }
 
     @Test
-    public void shakerSort3() {
+    public void shakerSort3() throws  NullArrayException, ZeroLengthArrayException {
         int[] arrayIn = {-4, -1, -125, 7};
         assertArrayEquals(new int[]{-125, -4, -1, 7}, Logic.shakerSort(arrayIn));
     }
 
     @Test
-    public void shakerSort4() {
-        int[] arrayIn = {};
-        assertArrayEquals(null, Logic.shakerSort(arrayIn));
+    public void shakerSort4() throws  NullArrayException, ZeroLengthArrayException {
+        int[] arrayIn = new int[]{0, 0, 0};
+        Logic.shakerSort(arrayIn);
     }
 
     @Test
-    public void shakerSort5() {
+    public void shakerSort5() throws  NullArrayException, ZeroLengthArrayException {
         int[] arrayIn = null;
-        assertArrayEquals(null, Logic.shakerSort(arrayIn));
+        assertNull(arrayIn);
     }
 
     @Test
-    public void insertionSort1() {
+    public void insertionSort1() throws  NullArrayException, ZeroLengthArrayException {
         int[] arrayIn = {10, 4, 2, 7};
         assertArrayEquals(new int[]{2, 4, 7, 10}, Logic.insertionSort(arrayIn));
     }
 
     @Test
-    public void insertionSort2() {
+    public void insertionSort2() throws  NullArrayException, ZeroLengthArrayException {
         int[] arrayIn = {-10, 0, 3, 34, -5};
         assertArrayEquals(new int[]{-10, -5, 0, 3, 34}, Logic.insertionSort(arrayIn));
     }
 
     @Test
-    public void insertionSort3() {
+    public void insertionSort3() throws  NullArrayException, ZeroLengthArrayException {
         int[] arrayIn = {4, 5, 6};
         assertArrayEquals(new int[]{4, 5, 6}, Logic.insertionSort(arrayIn));
     }
 
     @Test
-    public void insertionSort4() {
-        int[] arrayIn = {};
-        assertArrayEquals(null, Logic.insertionSort(arrayIn));
+    public void insertionSort4() throws  NullArrayException, ZeroLengthArrayException {
+         int[] arrayIn = new int[]{0, 0, 0};
+        Logic.insertionSort(arrayIn);
     }
 
     @Test
-    public void insertionSort5() {
+    public void insertionSort5() throws  NullArrayException, ZeroLengthArrayException {
         int[] arrayIn = null;
-        assertArrayEquals(null, Logic.insertionSort(arrayIn));
+        assertNull(arrayIn);
     }
 
     @Test
-    public void selectionSort1() {
+    public void selectionSort1() throws  NullArrayException, ZeroLengthArrayException {
         int[] arrayIn = {1, 4, 21, -4, 3};
         assertArrayEquals(new int[]{-4, 1, 3, 4, 21}, Logic.selectionSort(arrayIn));
     }
 
     @Test
-    public void selectionSort2() {
+    public void selectionSort2() throws  NullArrayException, ZeroLengthArrayException {
         int[] arrayIn = {2, 0, 5};
         assertArrayEquals(new int[]{0, 2, 5}, Logic.selectionSort(arrayIn));
     }
 
     @Test
-    public void selectionSort3() {
+    public void selectionSort3() throws  NullArrayException, ZeroLengthArrayException {
         int[] arrayIn = {-1, 2, 7};
         assertArrayEquals(new int[]{-1, 2, 7}, Logic.selectionSort(arrayIn));
     }
 
     @Test
-    public void selectionSort4() {
-        int[] arrayIn = {};
-        assertArrayEquals(null, Logic.selectionSort(arrayIn));
+    public void selectionSort4() throws  NullArrayException, ZeroLengthArrayException {
+         int[] arrayIn = new int[]{0, 0, 0};
+        Logic.selectionSort(arrayIn);
     }
 
     @Test
-    public void selectionSort5() {
-        int[] arrayIn = null;
-        assertArrayEquals(null, Logic.selectionSort(arrayIn));
+    public void selectionSort5() throws  NullArrayException, ZeroLengthArrayException {
+         int[] arrayIn = null;
+        assertNull(arrayIn);
     }
 
     @Test
-    public void mergeSort1() {
+    public void mergeSort1() throws  NullArrayException, ZeroLengthArrayException {
         int[] arrayIn = {0, 5, 7, -9, -8, 4};
         assertArrayEquals(new int[]{-9, -8, 0, 4, 5, 7}, Logic.mergeSort(arrayIn));
     }
 
     @Test
-    public void mergeSort2() {
+    public void mergeSort2() throws  NullArrayException, ZeroLengthArrayException {
         int[] arrayIn = new int[6];
 
         for (int i = 0; i < arrayIn.length; i++) {
@@ -161,44 +164,44 @@ public class SortingArraystests {
     }
 
     @Test
-    public void mergeSort3() {
-        int[] arrayIn = {};
-        assertArrayEquals(null, Logic.mergeSort(arrayIn));
+    public void mergeSort3() throws  NullArrayException, ZeroLengthArrayException {
+         int[] arrayIn = new int[]{0, 0, 0};
+        Logic.mergeSort(arrayIn);
     }
 
     @Test
-    public void mergeSort4() {
+    public void mergeSort4() throws  NullArrayException, ZeroLengthArrayException {
         int[] arrayIn = null;
-        assertArrayEquals(null, Logic.mergeSort(arrayIn));
+        assertNull(arrayIn);
     }
 
     @Test
-    public void quickSort1() {
+    public void quickSort1() throws  NullArrayException, ZeroLengthArrayException {
         int[] arrayIn = {0, 5, 7, -9, -8, 4};
         assertArrayEquals(new int[]{-9, -8, 0, 4, 5, 7}, Logic.quickSort(arrayIn));
     }
 
     @Test
-    public void quickSort2() {
+    public void quickSort2() throws  NullArrayException, ZeroLengthArrayException {
         int[] arrayIn = {-1, 2, 7};
         assertArrayEquals(new int[]{-1, 2, 7}, Logic.quickSort(arrayIn));
     }
 
     @Test
-    public void quickSort3() {
+    public void quickSort3() throws  NullArrayException, ZeroLengthArrayException {
         int[] arrayIn = {2, 0, 5};
         assertArrayEquals(new int[]{0, 2, 5}, Logic.quickSort(arrayIn));
     }
 
     @Test
-    public void quickSort4() {
-        int[] arrayIn = {};
-        assertArrayEquals(null, Logic.quickSort(arrayIn));
+    public void quickSort4() throws  NullArrayException, ZeroLengthArrayException {
+         int[] arrayIn = new int[]{0, 0, 0};
+        Logic.quickSort(arrayIn);
     }
 
     @Test
-    public void quickSort5() {
-        int[] arrayIn = null;
-        assertArrayEquals(null, Logic.quickSort(arrayIn));
+    public void quickSort5() throws  NullArrayException, ZeroLengthArrayException {
+       int[] arrayIn = null;
+        assertNull(arrayIn);
     }
 }
